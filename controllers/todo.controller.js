5ecd5cbc5c1b8700beed56a571d8435d72a741e9
NerfedJabolo <ezdlprofit1@gmail.com>
2025-06-06 16:06:36 +0300

    Created TodoController.updateTodo unit test and integration tests

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -30,4 +30,26 @@ const getTodoById = async (req, res, next) => {
   }
 };
 
-module.exports = { createTodo, getTodos, getTodoById };
+const updateTodo = async (req, res, next) => {
+  console.log('Updating ID:', req.params.todoId);
+
+  try {
+    const updatedTodo = await TodoModel.findByIdAndUpdate(
+      req.params.todoId,
+      req.body,
+      {
+        new: true,
+        useFindAndModify: false,
+      }
+    );
+    if (updatedTodo) {
+      res.status(200).json(updatedTodo);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createTodo, getTodos, getTodoById, updateTodo };
